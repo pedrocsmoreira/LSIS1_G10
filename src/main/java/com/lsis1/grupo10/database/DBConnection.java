318beabacd2b83debef33742a563b2415bd9eb99
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-06-26 22:12:19 +0100

    database class

---
+++ src/main/java/com/lsis1/grupo10/database/DBConnection.java
@@ -0,0 +1,30 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import keys.keys;
+/**
+ * It's a class that connects to the database
+ */
+public class DBConnection {
+
+    static final String DBNAME = keys.DBNAME;
+    static final String URL = keys.URL;
+    static final String USER = keys.USER;
+    static final String PASS = keys.PASS;
+
+    /**
+     * It creates a connection to the database
+     * 
+     * @return A connection to the database.
+     */
+    public static Connection getConnection() {
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException exc) {
+            throw new RuntimeException("Error connecting!", exc);
+        }
+    }
+}
